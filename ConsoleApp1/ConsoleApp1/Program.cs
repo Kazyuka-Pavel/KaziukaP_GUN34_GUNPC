2658bdb8e98865a6d940f2ddbcd80ad5b7f0e49d
KaziukaPP <pvlpka@yandex.ru>
2025-01-22 22:34:00 +0300

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,61 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Immutable;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        if (int.TryParse(Console.ReadLine(), out int a))
-        {
-            // обработка
-        }
-        else
-        {
-            Console.WriteLine("Not a number!!!");
-            return;
-        }
+        //int[] testArray = new[] { 1, 2, 3, };
+        //Array.Sort(testArray, 0 , 3); // сортировка
+        //Array.Reverse(testArray); //  наоборот        
+        //var a = Array.Empty<int>(); // создание пустого массива
+        //testArray.Length // длина
+        int[] array1 = { 5546, 342, 4545 };
+        float[] array2 = { 12.22, 34.56, 45.65 };
 
-        if (int.TryParse(Console.ReadLine(), out int b))
-        {
-            // обработка
-        }
-        else
-        {
-            Console.WriteLine("Not a number!!!");
-            return;
-        }
+        var result = Array.CreateInstance(typeof(float), 4);
+        result.SetValue(array1[0] * array2[0], 0);
+        result.SetValue(array1[0] / array2[0], 1);
 
-        uint c = (uint)a;
+        Console.WriteLine(result.GetValue(0));
+        Console.WriteLine(result.GetValue(1));
 
-        Console.WriteLine("Enter &, | or ^:");
+        Array.Sort(array2);
 
-        var s = Console.ReadLine();
-        if (s.Length == 0 || s.Length > 1)
-        {
-            Console.WriteLine("Wrong sign");
-            return;
-        }
-
-        switch (s[0])
-        {
-            case '&':
-                Console.WriteLine("Result of  {0} & {1} = {2}", a, b, a & b);
-                Console.WriteLine("0b result of  {0} & {1} = {2}", Convert.ToString(a, 2), Convert.ToString(b, 2), Convert.ToString(a & b, 2));
-                Console.WriteLine("0x Result of  {0} & {1} = {2}", Convert.ToString(a, 16), Convert.ToString(b, 16), Convert.ToString(a & b, 16));
-                break;
-            case '|':
-                Console.WriteLine("Result of  {0} | {1} = {2}", a, b, a | b);
-                Console.WriteLine("0b result of  {0} | {1} = {2}", Convert.ToString(a, 2), Convert.ToString(b, 2), Convert.ToString(a | b, 2));
-                Console.WriteLine("0x Result of  {0} | {1} = {2}", Convert.ToString(a, 16), Convert.ToString(b, 16), Convert.ToString(a | b, 16));
-                break;
-            case '^':
-                Console.WriteLine("Result of  {0} ^ {1} = {2}", a, b, a ^ b);
-                Console.WriteLine("0b result of  {0} ^ {1} = {2}", Convert.ToString(a, 2), Convert.ToString(b, 2), Convert.ToString(a ^ b, 2));
-                Console.WriteLine("0x Result of  {0} ^ {1} = {2}", Convert.ToString(a, 16), Convert.ToString(b, 16), Convert.ToString(a ^ b, 16));
-                break;
-            default:
-                Console.WriteLine("Wrong sign");
-                return;
-
-        }
+        Console.WriteLine(array2.Max());
+        Console.WriteLine(array2.Min());
     }
 }
