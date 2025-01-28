5848769fb84a44dedca758e4cff94fef3548a3f2
KaziukaPP <pvlpka@yandex.ru>
2025-01-29 00:58:01 +0300

    ex3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,39 +7,66 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var random = new Random();
-        int[] player1 = new[] { random.Next(100), random.Next(100), random.Next(100), random.Next(100) };
-        int[] player2 = new[] { random.Next(100), random.Next(100), random.Next(100), random.Next(100) };
-
-        int player1Score = 0;
-        int player2Score = 0;
-
-        for (int i = 0; i < player1.Length; i++)
+        // Числа Фибоначчи
+        var array1 = new int[10];
+        array1[0] = 0;
+        array1[1] = 1;
+        int i = 2;
+        Console.Write("1 .Числа Фибоначчи: ");
+        while (i < array1.Length)
         {
-            Console.WriteLine(player1[i] + " vs " + player2[i]);
-            if (player1[i] > player2[i])
+            array1[i] = array1[i - 1] + array1[i - 2];
+            Console.Write(array1[i] + " ");
+            i++;
+            //continue;
+            //break;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //Все четные 
+        Console.Write("2 .Все четные: ");
+        i = 0;
+        for (i = 0; i <= 20; i = i+2)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //Таблица умножения
+        Console.Write("3 .Таблица умножения: ");
+        Console.WriteLine();
+        for (i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++) 
             {
-                player1Score++;
+                Console.Write(i * j + "\t");
             }
-            else if (player1[i] < player2[i])
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //Чтение пароля
+        Console.Write("4 .Ввод пароля: ");
+        Console.WriteLine();
+        string password1 = "qwerty";
+        string password2 = "";
+        do
+        {
+            if (password2 != "")
             {
-                player2Score++;
+                Console.WriteLine("Неверный пароль! Попробуйте еще раз!");
             }
-        }
+            Console.Write("Введите пароль: ");
+            password2 = Console.ReadLine();
 
-        if (player1Score > player2Score)
-        {
-            Console.WriteLine("Player 1 won!");
-        }
-        else if (player1Score < player2Score)
-        {
-            Console.WriteLine("Player 2 won!");
-        }
-        else
-        {
-            Console.WriteLine("No winners!");
-        }
-
+        } while (password1 != password2);
+        Console.WriteLine("Верный пароль!");
     }
 
 }
