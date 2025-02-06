8cb7631436bb4d239ed0a9263789e60658fe72aa
KaziukaPP <pvlpka@yandex.ru>
2025-02-06 23:44:19 +0300

    Списки

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -44,6 +44,93 @@ internal class Program
             Console.WriteLine(next.Value);
             next = next.NextNode;
         }
+
+        Console.WriteLine();
+
+        // Стэк
+        // Послежний элемент выходит первым
+        var stack = new Stack<int>();
+        stack.Push(1); //Добавление в стэк
+        stack.Push(2);
+        stack.Push(3);
+
+        while(stack.Count > 0)
+        {
+            Console.WriteLine(stack.Pop()); //Извлечение из стэка
+        }
+
+        Console.WriteLine();
+
+        // Очередь 
+        var queue = new Queue<float>();
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+
+        while (queue.Count > 0 )
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+
+        Console.WriteLine();
+
+        // Dictionary
+        // Нет индексатора
+        //var dictionary = new Dictionary<string, object>(); //string - тип ключа object - тип значения
+        var dictionary = new Dictionary<int, string>();
+        dictionary.Add(1, "One");
+        dictionary.Add(2, "Two");
+        dictionary.Add(3, "Three");
+        
+        // Установка значения с првоеркой на уникальность ключа
+        if (dictionary.TryAdd(3, "Three"))
+        {
+            Console.WriteLine("Success");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+
+        //Получаение значение с проверкой на уникальность ключа
+        if (dictionary.TryGetValue(2, out string value))
+        {
+            Console.WriteLine($"{value}");
+        }
+
+        //Вывод значения по ключу
+        Console.WriteLine(dictionary[1]);
+
+        //Обход элементов словаря 
+        foreach (var kvp in dictionary)
+        {
+            Console.WriteLine("key = {0} and value = {1}",kvp.Key,kvp.Value);
+        }
+
+        Console.WriteLine();
+
+        //HashSet
+        var hashSet = new HashSet<int>();
+        hashSet.Add(0);
+        hashSet.Add(1);
+
+        foreach (var hash in hashSet)
+        {
+            Console.WriteLine(hash);
+        }
+
+        hashSet.Add(1); // тут может возникнуть исключение
+
+        var xhashSet = new HashSet<Node>();
+        var xnode1 = new Node() { Value = 5 };
+        var xnode2 = new Node() { Value = 5 };
+        xhashSet.Add(xnode1); //Т.к. это объект, то он добавляется по ХэщКоду
+        xhashSet.Add(xnode2); //Т.к. это объект, то он добавляется по ХэщКоду
+        foreach (var xhash in xhashSet)
+        {
+            Console.WriteLine(xhash.Value);
+        }
     }
 
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3148f4d8e6c5351ee4fb48831fade9941f5758")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85d9d96fff203f8d62164be6f2c011377317d05")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
