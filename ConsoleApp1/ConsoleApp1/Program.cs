f75b091d99cc107fc49a282beab511e8ce022a7b
KaziukaPP <pvlpka@yandex.ru>
2025-02-13 00:28:35 +0300

    ex7

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,122 +1,98 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml;
-using System.Xml.Serialization;
+using Xunit;
 
 internal class Program
 {
-    static string[] _categories = {"Numbers", "Animals"};
-    const string Value = "Const";
-
-    public static string XorEncrypt(string input, string key)
+    private static string ConcatenateStrings(string st1, string st2)
     {
-        char[] data = input.ToCharArray();
-        char[] keyData = key.ToCharArray();
-        char[] result = new char[data.Length];
-
-        for (int i = 0; i < data.Length; i++)
-        {
-            result[i] = (char)(data[i] ^ keyData[i % keyData.Length]);
-        }
-
-        return new String(result);
+        var sb = new StringBuilder();
+        sb.Append(st1);
+        sb.Append(st2);
+        return sb.ToString();
     }
 
-    public static string XorDecrypt(string input, string key)
+    private static string GreetUser(string name, int age)
     {
-        // Дешифрование очень похоже на шифрование, поскольку XOR - это обратимая операция.
-        return XorEncrypt(input, key);
+        return $"Hello, {name}! \nYou are {age} years old!";
+    }
+    private static string GetLengthLowerUpperString(string st)
+    {
+        return $"Length: {st.Length}\nLower: {st.ToLower()}\nUpper: {st.ToUpper()}";
+    }
+
+    private static string GetSubstring(string st)
+    {
+        return st.Substring(0, Math.Min(st.Length, 5));       
+    }
+
+    private static StringBuilder GetStringBuilder(string[] st)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < st.Length; i++)
+        {
+            sb.Append(st[i]);
+            if (i < st.Length - 1)
+            {
+                sb.Append(' ');
+            }            
+        }       
+        return sb;
+    }
+
+    private static string ReplaceWords(string inputString, string wordToReplace, string replacementWord)
+    {
+        return inputString.Replace(wordToReplace, replacementWord);
     }
 
     private static void Main(string[] args)
     {
-        // String 
-        var myStr = "My String\n";
-        var c = myStr.Insert(3,"First "); // вставить
-        string[] a = myStr.Split(' '); // разбить 
-        var b = myStr.Substring(6, 2); // вырезать
-        Console.WriteLine(myStr);
-        Console.WriteLine(b);
-        Console.WriteLine(c);
-        Console.WriteLine(c + b);
-
+        var result = "";
+        // 1.
+        Console.WriteLine("1. Concatenate strings");
+        result = ConcatenateStrings("Hello ", "Word");
+        Console.WriteLine(result);
+        Console.WriteLine($"Check: {"Hello Word".Equals(result)}");
         Console.WriteLine();
 
-        // Char
-        var d = 'f';
-        
-        Console.WriteLine(char.IsLetter(d));
-        Console.WriteLine(char.GetNumericValue(d));
-
+        // 2.
+        Console.WriteLine("2. Method GreetUser");
+        result = GreetUser("Vera", 5);
+        Console.WriteLine(result);
+        Console.WriteLine($"Check: {"Hello, Vera! \nYou are 5 years old!".Equals(result)}");
         Console.WriteLine();
 
-        // Регулярные выражения
-        var reg = "[a-z]";
-        var rx = new Regex(reg);
-        Console.WriteLine(rx.IsMatch("asd"));
-        Console.WriteLine(rx.IsMatch("123"));
-
+        // 3.
+        Console.WriteLine("3. Length Lower Upper");
+        result = GetLengthLowerUpperString("Hello");
+        Console.WriteLine(result);
+        Console.WriteLine($"Check: {"Length: 5\nLower: hello\nUpper: HELLO".Equals(result)}");
         Console.WriteLine();
 
-        // StringBuilder
-        var str1 = "Hello";
-        var str2 = "World";
-        var str3 = String.Empty;
-        var builder = new StringBuilder();
-        builder.Append(str1);
-        builder.Append(' ');
-        builder.Append(str2); 
-        builder.Append(str3);
-        Console.WriteLine(builder.ToString());
-
+        // 4. 
+        Console.WriteLine("4. Substring");
+        result = GetSubstring("Hello world");
+        Console.WriteLine(result);
+        Console.WriteLine($"Check: {"Hello".Equals(result)}");
+        result = GetSubstring("");
+        Console.WriteLine(result);
+        Console.WriteLine($"Check: {"Hello".Equals(result)}");
         Console.WriteLine();
 
-        // 
-        string originalText = "Привет, мир!";
-        string key = "757f87f87343dffdf3g";
-        string encryptedText = XorEncrypt(originalText, key);
-        Console.WriteLine($"Зашифрованный текст: {encryptedText}");
+        // 5. 
+        Console.WriteLine("5. StringBuilder");
+        string[] st3 = { "Starting", "to", "work"};
+        result = GetStringBuilder(st3).ToString();
+        Console.WriteLine(result);
+        Console.WriteLine($"Check: {"Starting to work".Equals(result)}");
+        Console.WriteLine();
 
-        string decryptedText = XorDecrypt(encryptedText, key);
-        Console.WriteLine($"Расшифрованный текст: {decryptedText}");
-
-        // Викторина 
-        var categories = new Dictionary<int, string>
-        {
-            {1, _categories[0]},
-            {2, _categories[1]}
-        };
-
-        var categoryQuestions = new Dictionary<int, Dictionary<string, string>>();
-        var questions = new Dictionary<string, string>() { { "PI?", "3.14" } };
-
-        categoryQuestions.Add(1,questions);
-        Console.WriteLine("Select categoty 1 or 2");
-        var selected = int.Parse(Console.ReadLine());
-        Console.WriteLine($"Your category {categories[selected]}");
-        var result = categoryQuestions[selected];
-        foreach (var category in result)
-        {           
-            Console.WriteLine(category.Key);
-            int attempst = 0;
-            while (true)
-            {
-                var answer = Console.ReadLine();
-                attempst++;
-                if (attempst >= 3 || answer  == category.Value )
-                {
-                    break;
-                }
-            }
-            if( attempst <= 3)
-            {
-                Console.WriteLine("you won");
-            }            
-            break;
-        }
-        
+        // 6.
+        Console.WriteLine("6. ReplaceWords");
+        result = ReplaceWords("Hello world", "world", "universe");
+        Console.WriteLine(result);
+        Console.WriteLine($"Check: { "Hello universe".Equals(result)}");
     }
 }
