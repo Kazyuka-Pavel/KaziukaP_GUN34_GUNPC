6d143d669899dc2371c3a711c4da07f377776e27
KaziukaPP <pvlpka@yandex.ru>
2025-03-12 09:17:50 +0300

    События

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,26 +2,53 @@
 {
     public delegate string ToStringConvert(Test param);
 
+    public delegate float MathFloat(float param1, float param2);
+
     public class Test
     {
+        public event MathFloat FloatCalculator;
+
         public Test(string someString) => SomeString = someString;
 
         private string SomeString { get; }
 
         public string Method(Test param) => param.ToString();
 
+        public void Calculate(float param1, float param2)
+        {
+            if (FloatCalculator != null)
+            {
+                Console.WriteLine(FloatCalculator.Invoke(param1, param2));
+            }
+        }
         public override string ToString() => $"This is test class with string {SomeString}";
     }
 
     internal class Program
     {
-          
+        public static float Sum(float param1, float param2) => param1 + param2;
+
+        public static float Mult(float param1, float param2) => param1 * param2;
+
+        private class MathTest()
+        {
+            public static float Mult(float param1, float param2) => param1 * param2;
+        }
+
+        public static void MemoryLeakTest(MathTest math)
+        {
+            var newTest = new Test("df");
+            newTest.FloatCalculator += Sum;
+        }
 
         static void Main(string[] args)
         {
             var test = new Test("Netology");
-            var del = new ToStringConvert(test.Method);
-            Console.WriteLine(del(test));
+            test.FloatCalculator += Sum;
+            test.Calculate(10f,13.3f);
+
+            //var del = new ToStringConvert(test.Method);
+            //Console.WriteLine(del(test));
         }
     }
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8290806fd671fa94fc77a8221206bd8d76e5899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b08687f638333564c634852f3804a55d1756896")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
