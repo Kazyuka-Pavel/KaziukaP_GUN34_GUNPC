3b08687f638333564c634852f3804a55d1756896
KaziukaPP <pvlpka@yandex.ru>
2025-03-12 08:09:46 +0300

    Делегаты

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    public delegate string ToStringConvert(Test param);
+
+    public class Test
+    {
+        public Test(string someString) => SomeString = someString;
+
+        private string SomeString { get; }
+
+        public string Method(Test param) => param.ToString();
+
+        public override string ToString() => $"This is test class with string {SomeString}";
+    }
+
+    internal class Program
+    {
+          
+
+        static void Main(string[] args)
+        {
+            var test = new Test("Netology");
+            var del = new ToStringConvert(test.Method);
+            Console.WriteLine(del(test));
+        }
+    }
+}
