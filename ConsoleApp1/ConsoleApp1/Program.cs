e3be93ed056c8a7e4f8ea703858a41e17c51ce19
KaziukaPP <pvlpka@yandex.ru>
2025-03-13 09:13:21 +0300

    Предикаты и делегаты

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,54 +1,40 @@
 ﻿namespace ConsoleApp1
 {
-    public delegate string ToStringConvert(Test param);
-
-    public delegate float MathFloat(float param1, float param2);
-
     public class Test
     {
-        public event MathFloat FloatCalculator;
-
-        public Test(string someString) => SomeString = someString;
-
-        private string SomeString { get; }
-
-        public string Method(Test param) => param.ToString();
-
-        public void Calculate(float param1, float param2)
+        public void ActionTest(Action action)
         {
-            if (FloatCalculator != null)
+            action?.Invoke(); // ? - спец оператор, который позволяет проверить ссылку на null, если есть ссылка, то выховет, в противном случае - нет
+        }
+
+        public void FuncTest(Func<int, int> func, int param) // в делегате первый всегда входной, а второй всегда возвращаемый
+        {
+            Console.WriteLine(func?.Invoke(param));
+        }
+
+        public void Predicatetest(Predicate<int> predicate, int param) 
+        {
+            if (predicate?.Invoke(param) == true)
             {
-                Console.WriteLine(FloatCalculator.Invoke(param1, param2));
+                Console.WriteLine("TRUE!");
             }
         }
-        public override string ToString() => $"This is test class with string {SomeString}";
     }
 
     internal class Program
     {
-        public static float Sum(float param1, float param2) => param1 + param2;
-
-        public static float Mult(float param1, float param2) => param1 * param2;
-
-        private class MathTest()
-        {
-            public static float Mult(float param1, float param2) => param1 * param2;
-        }
-
-        public static void MemoryLeakTest(MathTest math)
-        {
-            var newTest = new Test("df");
-            newTest.FloatCalculator += Sum;
-        }
-
         static void Main(string[] args)
         {
-            var test = new Test("Netology");
-            test.FloatCalculator += Sum;
-            test.Calculate(10f,13.3f);
+            // Делегат
+            var test = new Test();
+            test.ActionTest(delegate { Console.WriteLine("Action anonymous");  });
+            test.ActionTest(() => Console.WriteLine("Action lambda"));
 
-            //var del = new ToStringConvert(test.Method);
-            //Console.WriteLine(del(test));
+            test.FuncTest((int param) => param * 5, 25);
+
+            // Предикат 
+            var test1 = new Test();
+            test1.Predicatetest((int param) => param > 5, 13);
         }
     }
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b08687f638333564c634852f3804a55d1756896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d143d669899dc2371c3a711c4da07f377776e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
